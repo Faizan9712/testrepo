2992a91936ac31f82811c7d8a128a5059377e696
Faizan9712 <faizu97123@gmail.com>
2023-03-09 18:46:18 +0530

    sada

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql");
 //MySQL details
 var mysqlConnection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
-  port:3306,
+  port: 3306,
   database: "learner",
-  multipleStatements: true,
 });
 
 module.exports = mysqlConnection;
