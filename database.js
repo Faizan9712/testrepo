b7365808896421b5a47586415d7722fbd4a51e8e
Faizan9712 <faizu97123@gmail.com>
2023-03-09 20:49:49 +0530

    dfgrft

--- database.js
+++ database.js
@@ -2,10 +2,11 @@ const mysql = require("mysql");
 //MySQL details
 
 const mysqlConnection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE, //nom de la database
+  host: "database-1.ct814wzajzwe.us-east-1.rds.amazonaws.com",
+  port:"3306",
+  user: "admin",
+  password: "faizan9712",
+  database: "my_db", //nom de la database
 });
 
 module.exports = mysqlConnection;
