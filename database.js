0dbcf9ccb535b0ba489d30e334892cbb7046aeaf
Faizan9712 <faizu97123@gmail.com>
2023-03-09 19:42:55 +0530

    sdfes

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 //MySQL details
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'learner', //nom de la database
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE, //nom de la database
 });
 
 module.exports = mysqlConnection;
