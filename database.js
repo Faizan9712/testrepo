ff165275a8744cae2a393e9ae33a80edfee1cd3a
Faizan9712 <faizu97123@gmail.com>
2023-03-09 18:52:44 +0530

    adwe

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 //MySQL details
 
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "learner", //nom de la database
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'learner', //nom de la database
 });
 
 module.exports = mysqlConnection;
