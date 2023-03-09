e6f02b6375a0647cdaca5ec1f63737fd83a25d3e
Faizan9712 <faizu97123@gmail.com>
2023-03-09 17:04:46 +0530

    db

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5001;
+const mysql = require("mysql");
+const bodyparser = require("body-parser");
+//Configuring express server
+app.use(bodyparser.json());
 
-app.get('/', (req, res) => {
-  res.json({message:'hey Faizn!'});
+//MySQL details
+var mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "learner",
+  multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+  if (!err) console.log("Connection Established Successfully");
+  else console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "hey Faizn!" });
+});
+
+app.get("/learners", (req, res) => {
+  mysqlConnection.query("SELECT * FROM learnerdetails", (err, rows, fields) => {
+    if (!err) res.json(rows);
+    else console.log(err);
+  });
 });
 
 app.listen(port, () => {
